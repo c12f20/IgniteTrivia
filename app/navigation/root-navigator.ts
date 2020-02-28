c2b8929af913204701172266b15b2a448f5c8bab
hill.cen <c12f20@gmail.com>
2020-02-28 11:31:22 +0800

    Add Login Screen

--- app/navigation/root-navigator.ts
+++ app/navigation/root-navigator.ts
@@ -1,9 +1,11 @@
 import { createStackNavigator } from "react-navigation-stack"
 import { PrimaryNavigator } from "./primary-navigator"
-import {} from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
+import {  LoginScreen,
+} from "../screens" // eslint-disable-line @typescript-eslint/no-unused-vars
 
 export const RootNavigator = createStackNavigator(
   {
+    loginScreen: { screen: LoginScreen },
     primaryStack: { screen: PrimaryNavigator },
   },
   {
---
+++ app/screens/login-screen.tsx
@@ -0,0 +1,55 @@
+import * as React from "react"
+import { observer } from "mobx-react-lite"
+import { ViewStyle, TextStyle } from "react-native"
+import { Screen, Text, Button } from "../components"
+// import { useStores } from "../models/root-store"
+import { color } from "../theme"
+import { NavigationInjectedProps } from "react-navigation"
+
+export interface LoginScreenProps extends NavigationInjectedProps<{}> {
+}
+
+const CONTAINER: ViewStyle = {
+  backgroundColor: color.palette.white,
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+
+const TITLE: TextStyle = {
+  height: 200,
+  color: color.palette.black,
+  fontSize: 32,
+  textAlign: 'center',
+  textAlignVertical: 'center',
+}
+
+const BUTTON: ViewStyle = {
+  width: 200,
+  height: 50,
+  margin: 10,
+}
+
+const BUTTON_TEXT: TextStyle = {
+  color: color.palette.black,
+  fontSize: 20,
+}
+
+export const LoginScreen: React.FunctionComponent<LoginScreenProps> = observer((props) => {
+  // const { someStore } = useStores()
+
+  const onSignIn = React.useMemo(() => () => {
+    console.tron.log("Sign In Pressed");
+  }, [props.navigation])
+
+  const onSignUp = React.useMemo(() => () => {
+    console.tron.log("Sign Up Pressed");
+  }, [])
+
+  return (
+    <Screen style={CONTAINER} preset="fixed">
+      <Text style={TITLE} tx="loginScreen.title" />
+      <Button style={BUTTON} textStyle={BUTTON_TEXT} tx="loginScreen.signin" onPress={onSignIn}/>
+      <Button style={BUTTON} textStyle={BUTTON_TEXT} tx="loginScreen.signup" onPress={onSignUp}/>
+    </Screen>
+  )
+})
